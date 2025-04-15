f9ec3e88f635565c3f85f1c7816711330dc62716
Yuval Ailon <yuval.ailon23@gmail.com>
2025-04-15 14:58:53 -0400

    cleaned up steganography code

--- Banana/src/cybersecurityDemonstration/Level3/BasicSteganography.java
+++ Banana/src/cybersecurityDemonstration/Level3/BasicSteganography.java
@@ -16,41 +16,18 @@ public class BasicSteganography {
     ImageManager.getImageFrom(extractMessageFromImage("src/cybersecurityDemonstration/Level3/Image/message.png"));
   }
 
-  private static void addBinaryTo(char character, List<Integer> binaryList){
-    String binaryString = String.format("%8s", Integer.toBinaryString(character)).replace(' ', '0');;
-    for (Character c : binaryString.toCharArray()) {
-      binaryList.add(Integer.parseInt(String.valueOf(c)));
-    }
-  }
-
-  private static int coordToIndex(int x, int y, int width){
-    return (y * width + x) * 3;
-  }
-
-  private static int binaryOrZero(int index, List<Integer> binary){
-    try {
-      return binary.get(index);
-    }catch (Exception ignored){
-      return 0;
-    }
-  }
-
-  private static Color AddSecretToColorValue(int r, int g, int b, int colorValue){
-    Color color = new Color(colorValue);
-    return new Color(
-            alterLSB(r, color.getRed()),
-            alterLSB(g, color.getGreen()),
-            alterLSB(b, color.getBlue())
-    );
-  }
-
-  private static int alterLSB(int secret, int colorValue){
-    String colorBinary = String.format("%8s", Integer.toBinaryString(colorValue)).replace(' ', '0');
-    colorBinary = colorBinary.substring(0, colorBinary.length() - 1) + secret;
-    return Integer.parseInt(colorBinary, 2);
-  }
-
-  private static void encodeMessageInImage(String message, String filepath, String messageName) throws IOException {
+  /**
+   * Encodes the given message in the given image.
+   * @param message the message
+   * @param filepath the filepath to the original image
+   * @param messageName the filepath of the image to be created
+   * @throws IOException if image cannot be read/witten
+   */
+  private static void encodeMessageInImage(
+          String message,
+          String filepath,
+          String messageName
+  ) throws IOException {
     List<Integer> binary = new ArrayList<>();
     for(char character : message.toCharArray()){
       addBinaryTo(character, binary);
@@ -77,6 +54,85 @@ public class BasicSteganography {
     ImageIO.write(imageWithMessage, "png", new File(messageName));
   }
 
+
+  /**
+   * Adds the binary encoding of a given character to the given list as a series of 1s and 0s.
+   * @param character the given character
+   * @param binaryList the given list
+   */
+  private static void addBinaryTo(char character, List<Integer> binaryList){
+    String binaryString = String.format(
+            "%8s",
+            Integer.toBinaryString(character)).replace(' ', '0'
+    );
+
+    for (Character c : binaryString.toCharArray()) {
+      binaryList.add(Integer.parseInt(String.valueOf(c)));
+    }
+  }
+
+  /**
+   * Given an (x,y) coordinate in an image, transforms it to the index of the bit to be hidden.
+   * at that coordinate
+   * @param x the x-coordinate
+   * @param y the y-coordinate
+   * @param width the width of the image
+   * @return the index of the bit to be hidden
+   */
+  private static int coordToIndex(int x, int y, int width){
+    return (y * width + x) * 3;
+  }
+
+  /**
+   * Returns either the bit at the specified location in the binary list, or a 0-bit
+   * @param index the index of the specified bit
+   * @param binary the binary array
+   * @return the bit at the given index, or 0 if it is out of bounds
+   */
+  private static int binaryOrZero(int index, List<Integer> binary){
+    try {
+      return binary.get(index);
+    }catch (Exception ignored){
+      return 0;
+    }
+  }
+
+  /**
+   * Adds three secret bits to a given color value.
+   * @param r the bit to be hidden in the red value
+   * @param g the bit to be hidden in the green value
+   * @param b the bit to be hidden in the blue value
+   * @param colorValue the color value
+   * @return the color value with the hidden 3 bits
+   */
+  private static Color AddSecretToColorValue(int r, int g, int b, int colorValue){
+    Color color = new Color(colorValue);
+    return new Color(
+            alterLSB(r, color.getRed()),
+            alterLSB(g, color.getGreen()),
+            alterLSB(b, color.getBlue())
+    );
+  }
+
+  /**
+   * Given a red, green, or blue value, alters its least significant bit to contain a secret.
+   * @param secret the secret
+   * @param colorValue the red, green, or blue color value
+   * @return the altered color value
+   */
+  private static int alterLSB(int secret, int colorValue){
+    String colorBinary = String.format("%8s", Integer.toBinaryString(colorValue)).replace(' ', '0');
+    colorBinary = colorBinary.substring(0, colorBinary.length() - 1) + secret;
+    return Integer.parseInt(colorBinary, 2);
+  }
+
+
+  /**
+   * Extracts a message from an image.
+   * @param filepath the filepath of the image with the secret message
+   * @return the string hidden in the image.
+   * @throws IOException if the image cannot be read
+   */
   private static String extractMessageFromImage(String filepath) throws IOException {
     File originalFile = new File(filepath);
     BufferedImage image = ImageIO.read(originalFile);
@@ -105,16 +161,27 @@ public class BasicSteganography {
     return message.toString();
   }
 
+  /**
+   * Adds the least significant bits of an RGB color value to a given binary list.
+   * @param color the RGB color value
+   * @param binary the given binary list
+   */
   private static void extractLSBTo(Color color, List<Integer> binary){
     binary.add(extractLSB(color.getRed()));
     binary.add(extractLSB(color.getGreen()));
     binary.add(extractLSB(color.getBlue()));
   }
 
+  /**
+   * Extrats the least significant bit from a red, green, or blue color value.
+   * @param colorValue the color value
+   * @return the LSB
+   */
   private static int extractLSB(int colorValue){
     String colorBinary = Integer.toBinaryString(colorValue);
-    return Integer.parseInt(String.valueOf(Integer.toBinaryString(colorValue).charAt(colorBinary.length() - 1)));
+    return Integer.parseInt(
+            String.valueOf(Integer.toBinaryString(colorValue).charAt(colorBinary.length() - 1))
+    );
   }
-
 
 }
